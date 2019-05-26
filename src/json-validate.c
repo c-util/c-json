c38d4f6760b803250627a23c24685bbbc0e9094b
Lars Karlitski <lars@karlitski.net>
2019-05-26 18:17:36 +0200

    Import tests from JSONTestSuite

--- src/json-validate.c
+++ src/json-validate.c
@@ -4,9 +4,38 @@
 #include <stdio.h>
 #include "c-json.h"
 
-int json_read_value(CJson *json) {
-        int r = 0;
+int read_file(FILE *file, char **contentsp) {
+        _c_cleanup_ (c_fclosep) FILE *stream = NULL;
+        _c_cleanup_ (c_freep) char *contents = NULL;
+        size_t n_input;
 
+        stream = open_memstream(&contents, &n_input);
+        if (!stream)
+                return -errno;
+
+        for (;;) {
+                char buffer[8192];
+                size_t n;
+
+                n = fread(buffer, 1, sizeof(buffer), file);
+                if (n == 0) {
+                        if (ferror(file))
+                                return -errno;
+                        break;
+                }
+
+                if (fwrite(buffer, 1, n, stream) != n)
+                        return -errno;
+        }
+
+        stream = c_fclose(stream);
+        *contentsp = contents;
+        contents = NULL;
+
+        return 0;
+}
+
+int json_read_value(CJson *json) {
         switch (c_json_peek(json)) {
                 case C_JSON_TYPE_NULL:
                         return c_json_read_null(json);
@@ -22,14 +51,20 @@ int json_read_value(CJson *json) {
 
                 case C_JSON_TYPE_ARRAY:
                         c_json_open_array(json);
-                        while (!r && c_json_more(json))
-                                r = json_read_value(json);
+                        while (c_json_more(json)) {
+                                int r = json_read_value(json);
+                                if (r)
+                                        return r;
+                        }
                         return c_json_close_array(json);
 
                 case C_JSON_TYPE_OBJECT:
                         c_json_open_object(json);
-                        while (!r && c_json_more(json))
-                                r = json_read_value(json);
+                        while (c_json_more(json)) {
+                                int r = json_read_value(json);
+                                if (r)
+                                        return r;
+                        }
                         return c_json_close_object(json);
 
                 default:
@@ -38,69 +73,30 @@ int json_read_value(CJson *json) {
 }
 
 int main(int argc, char **argv) {
-        _c_cleanup_ (c_json_freep) CJson *json = NULL;
         _c_cleanup_ (c_fclosep) FILE *file = NULL;
-        _c_cleanup_ (c_fclosep) FILE *stream = NULL;
+        _c_cleanup_ (c_json_freep) CJson *json = NULL;
         _c_cleanup_ (c_freep) char *input = NULL;
-        size_t n_input;
         int r;
 
         if (argc < 2) {
-                fprintf(stderr, "Usage: %s filename\n", argv[0]);
-                return 1;
-        }
-
-        stream = open_memstream(&input, &n_input);
-        if (!stream) {
-                perror("Error: ");
-                return 1;
-        }
-
-        file = fopen(argv[1], "r");
-        if (!file) {
-                perror("Error: ");
-                return 1;
-        }
-
-        for (;;) {
-                char buffer[8192];
-                int n;
-
-                n = fread(buffer, 1, sizeof(buffer), file);
-                if (n <= 0) {
-                        if (ferror(file)) {
-                                perror("Error: ");
-                                return 1;
-                        }
-                        break;
-                }
-
-                if (fwrite(buffer, 1, n, stream) != n) {
-                        perror("Error: ");
+                r = read_file(stdin, &input);
+                if (r)
                         return 1;
-                }
+        } else {
+                file = fopen(argv[1], "r");
+                if (!file)
+                        return -errno;
         }
 
-        stream = c_fclose(stream);
+        r = read_file(file, &input);
+        if (r)
+                return 1;
 
         c_json_new(&json, 256);
         c_json_begin_read(json, input);
-        json_read_value(json);
-        r = c_json_end_read(json);
-        if (r) {
-                /* JSONTestSuite's test cases start with 'y' (valid), 'n' (invalid), or i (either) */
-                switch (r) {
-                        case C_JSON_E_INVALID_JSON:
-                        case C_JSON_E_DEPTH_OVERFLOW: {
-                                char *filename = basename(argv[1]);
-                                if (filename[0] == 'n' || filename[0] == 'i')
-                                        return 0;
-                        }
+        r = json_read_value(json);
+        if (r)
+                return r;
 
-                        default:
-                                return 1;
-                }
-        }
-
-        return 0;
+        return c_json_end_read(json);
 }
